f530eb19966ee89634281911c67c9c04c742a571
thermarthae <thermarthae@protonmail.com>
2022-12-17 21:02:52 +0100

    Replace microbundle with rollup

---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+import dts from 'rollup-plugin-dts';
+import { swc } from 'rollup-plugin-swc3';
+
+import pkg from './package.json' assert { type: 'json' };
+
+/**  @type {import('rollup').RollupOptions} */
+const commonConfig = {
+	input: './src/index.ts',
+	external: [
+		...Object.keys(pkg.dependencies),
+		...Object.keys(pkg.peerDependencies),
+	],
+	output: {
+		dir: './dist',
+		preserveModules: true,
+		sourcemap: true,
+	},
+	plugins: [
+		swc({
+			jsc: {
+				target: 'es2022',
+			},
+		}),
+	],
+};
+
+/**  @type {import('rollup').RollupOptions[]} */
+export default [
+	// *.js
+	{
+		...commonConfig,
+		output: {
+			...commonConfig.output,
+			format: 'esm',
+		},
+	},
+	// *.cjs
+	{
+		...commonConfig,
+		output: {
+			...commonConfig.output,
+			format: 'cjs',
+			entryFileNames: '[name].cjs',
+		},
+	},
+	// *.d.ts
+	{
+		...commonConfig,
+		output: {
+			...commonConfig.output,
+			sourcemap: false,
+		},
+		plugins: [dts()],
+	},
+];
