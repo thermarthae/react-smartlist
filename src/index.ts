3c5fce92c3f58c7c743a8bfcf1823eab99f2be87
thermarthae <thermarthae@protonmail.com>
2022-12-17 19:58:44 +0100

    Remove `shallowDiffers` from the main entrypoint

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export { default } from './VirtualList';
 export type { TProps as TVirtualListProps } from './VirtualList';
 
 export type { TChildrenProps } from './VirtualListItem';
-
-export { default as shallowDiffers } from './shallowDiffers';
