bf789843f80511b3d026a827f2250cb4368ba559
thermarthae <thermarthae@protonmail.com>
2023-06-21 22:31:29 +0200

    Update yarn and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,3 @@
 module.exports = {
 	extends: ['@thermarthae/eslint-config'],
-	ignorePatterns: [
-		'.yarn/*',
-	],
-	rules: {
-		'linebreak-style': ['error', 'windows'],
-		'react/sort-comp': 0,
-		'react/react-in-jsx-scope': 0,
-	},
 };
