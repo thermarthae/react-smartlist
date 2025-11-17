b0b533d1b94d28c31fa53e2d053834cc97296471
thermarthae <thermarthae@protonmail.com>
2025-11-17 13:22:06 +0100

    Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 
 export default defineConfig({
 	root: './src',
